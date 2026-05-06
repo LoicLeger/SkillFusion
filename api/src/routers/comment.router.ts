131759f170e82d7f1eed7b945c90e28c2c60ddae
Samed Celik <samed.celik@oclock.school>
2026-05-06 15:12:27 +0200

    Add button for report a comment

--- api/src/routers/comment.router.ts
+++ api/src/routers/comment.router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import commentController from "../controllers/comment.controller";
 import { verifyToken } from '../middlewares/auth.middleware';
 import { checkRoles, requireSelfOrAdmin, ROLES } from '../middlewares/rbac.middleware';
+import { sendReportEmail } from "../services/mailer";
 
 const router = express.Router();
 
@@ -14,5 +15,7 @@ router.post("/comments", verifyToken, checkRoles([ROLES.STUDENT, ROLES.INSTRUCTO
 router.patch("/comments/:id", verifyToken,  commentController.updatingComment)
 
 router.delete("/comments/:id", verifyToken, commentController.deleteComment)
+
+router.post("/comments/:id/report", verifyToken, commentController.reportComment)
 
 export default router;
